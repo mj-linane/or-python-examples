526deeb291eaefd7976f0c44b759e41caf41d263
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-01-19 13:09:00 -0500

    Update Robot Acceleration.py

--- Robot Acceleration.py
+++ Robot Acceleration.py
@@ -1,59 +1,7 @@
-# https: // programmingelectronics.com/3-ways-to-use-acceleration-in-an-arduino-sketch
 import math
 import time
 
 
-# def AccelerationTo(current_power, MaxSpeed):
-#     time = 1
-#     growth_rate = 1.03  # Growth Rate
-#     if current_power < 1:
-#         current_power = 1
-#     while current_power < MaxSpeed:
-#         # Calculation of acceleration using doubling time
-#         Increase = current_power * (growth_rate ** time)
-#         current_power = current_power+Increase
-#         if current_power > MaxSpeed:
-#             current_power = MaxSpeed
-#         time += 1
-#         print("Speed: ", current_power)
-#         print("time:", time)
-
-
-# def DeaccelerationTo(current_power, min_power):
-#     time = 1
-#     growth_rate = 1.03  # Growth Rate
-#     if current_power < 0:
-#         while current_power < min_power:
-#             # Calculation of acceleration using doubling time
-#             current_power = current_power + \
-#                 (current_power * math.ceil(growth_rate ** time))
-#             print(current_power)
-#             time += 1
-#     else:  # Current Power >= 0
-#         while current_power > min_power:
-#             current_power = current_power - \
-#                 (current_power * math.ceil(growth_rate ** time))
-#             time += 1
-#         print("Speed: ", current_power)
-#         print("time:", time)
-
-
-# def AccelerationToV2(current_power, MaxSpeed):
-#     time = 1
-#     growth_rate = 1.03  # Growth Rate
-
-#     if current_power < 1:
-#         current_power = 1
-#     while current_power < MaxSpeed:
-#         current_power = current_power*(growth_rate**time)
-#         # Calculation of acceleration using doubling time
-#         if current_power > MaxSpeed:
-#             current_power = MaxSpeed
-#         time += 1
-#         print("Speed: ", current_power)
-#         print("time:", time)
-
-# FINAL
 def deaccelerate_to(current_power, min_power):
     time = 1
     growth_rate = .9  # Growth Rate
@@ -68,10 +16,7 @@ def deaccelerate_to(current_power, min_power):
         time += 1
         print("Speed: ", current_power)
         print("time:", time)
-
-# FINAL
-
-
+        
 def forward_accelerate_to(current_power, max_power):
     time = 1
     growth_rate = 1.03  # Growth Rate
@@ -90,9 +35,6 @@ def forward_accelerate_to(current_power, max_power):
         print("Speed: ", current_power)
         print("time:", time)
 
-# FINAL
-
-
 def reverse_accelerate_to(current_power, max_power):
     time = 1
     growth_rate = 1.03  # Growth Rate
@@ -110,6 +52,4 @@ def reverse_accelerate_to(current_power, max_power):
         time += 1
         print("Speed: ", current_power)
         print("time:", time)
-
-
-forward_accelerate_to(0, 100)
+        
